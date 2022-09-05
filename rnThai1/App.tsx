d900685e40af10dbc9420bc08ab0a2652c89d878
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-05 12:00:57 +0700

    1234

--- rnThai1/App.tsx
+++ rnThai1/App.tsx
@@ -1,12 +1,13 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import HomeScreen from './src/HomeScreen';
 
 type Props = {};
 
 const App = (props: Props) => {
   return (
     <View>
-      <Text>App</Text>
+      <HomeScreen />
     </View>
   );
 };
