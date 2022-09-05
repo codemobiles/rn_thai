87112207563795f9ce87353baa0dd7541836d10f
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-05 13:36:12 +0700

    1234

--- rnThai1/backend/readme.txt
+++
@@ -1,7 +0,0 @@
-npx nodemon server.js
-npm init -y
-yarn add express formidable fs-extra
-
-http://localhost:3000/
-
-{"username":"admin", "password": "1234"}
--- rnThai1/src/HomeScreen.tsx
+++ rnThai1/src/HomeScreen.tsx
@@ -7,11 +7,6 @@ const HomeScreen = (props: Props) => {
   return (
     <View>
       <Text>HomeScreen</Text>
-      <Image
-        style={{width: '100%'}}
-        resizeMode="contain"
-        source={require('./assets/img/dog.jpeg')}
-      />
     </View>
   );
 };
