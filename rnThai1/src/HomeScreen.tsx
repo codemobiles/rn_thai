8d2bd723aa0929e841a66483631a36d9d8061b28
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-05 12:04:36 +0700

    1234

--- rnThai1/src/HomeScreen.tsx
+++ rnThai1/src/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
 
 type Props = {};
@@ -7,6 +7,11 @@ const HomeScreen = (props: Props) => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Image
+        style={{width: '100%'}}
+        resizeMode="contain"
+        source={require('./assets/img/dog.jpeg')}
+      />
     </View>
   );
 };
