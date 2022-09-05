e483116a3ea2a37291a87aa04d00dd0d8231cdae
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-05 11:55:04 +0700

    1234

---
+++ rnThai1/src/JSONFeedScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const JSONFeedScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>JSONFeedScreen</Text>
+    </View>
+  )
+}
+
+export default JSONFeedScreen
