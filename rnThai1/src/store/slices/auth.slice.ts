27ba9cc56e743673a1b4ad3f59329d838000954e
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-06 15:26:47 +0700

    1234

--- rnThai1/src/store/slices/auth.slice.ts
+++ rnThai1/src/store/slices/auth.slice.ts
@@ -1,18 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {RootState} from '../store';
+
+type authState = {
+  count: number;
+};
+
+const defaultState: authState = {
+  count: 0,
+};
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState: {count: 0},
+  initialState: defaultState,
   reducers: {
     add: state => {
       state.count = state.count + 1;
+    },
+    reset: (state: authState, action: PayloadAction<number>) => {
+      state.count = action.payload;
     },
   },
   extraReducers: builder => {},
 });
 
-export const {add} = authSlice.actions;
+export const {add, reset} = authSlice.actions;
 export const authSelector = (state: RootState) => state.authReducer;
 export default authSlice.reducer;
