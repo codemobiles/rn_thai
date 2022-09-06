e57207a8620b77401fc4945f75ba2e11d456f4e9
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-06 14:49:38 +0700

    1234

--- rnThai1/index.js
+++ rnThai1/index.js
@@ -8,12 +8,15 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {NavigationContainer} from '@react-navigation/native';
-
+import {Provider} from 'react-redux';
+import {store} from './src/store/store';
 // Hoc
 const NavApp = () => (
-  <NavigationContainer>
-    <App />
-  </NavigationContainer>
+  <Provider store={store}>
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  </Provider>
 );
 
 AppRegistry.registerComponent(appName, () => NavApp);
