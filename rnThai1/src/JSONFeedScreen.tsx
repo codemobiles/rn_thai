91f9ae44bc1d932c8b740063c73dcf4fdae5f425
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-06 11:54:08 +0700

    1234

--- rnThai1/src/JSONFeedScreen.tsx
+++ rnThai1/src/JSONFeedScreen.tsx
@@ -1,14 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import axios from 'axios';
 
-type Props = {}
+type Props = {};
 
 const JSONFeedScreen = (props: Props) => {
+  const courses = ['java', 'flutter', 'angular', 'react'];
+  const [dataArray, setDataArray] = React.useState<any[]>([]);
+
+  React.useEffect(() => {
+    console.log('JSONScreen is created');
+    // loadData();
+  }, []);
+
+  const loadData = async () => {
+    const url =
+      'https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=songs';
+    const result = await axios.get(url);
+    setDataArray(result.data.youtubes);
+  };
+
   return (
     <View>
-      <Text>JSONFeedScreen</Text>
+      {dataArray.map(item => (
+        <Text style={{fontSize: 20}} key={item.id}>
+          {item.title}
+        </Text>
+      ))}
+      {/* {courses.map(item => (
+        <Text style={{fontSize: 70}} key={item}>
+          {item}
+        </Text>
+      ))} */}
     </View>
-  )
-}
+  );
+};
 
-export default JSONFeedScreen
+export default JSONFeedScreen;
