b6ff4bc8a64bf6eac47001c1c0d3e0c5c6950d21
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-06 10:30:41 +0700

    1234

--- rnThai1/src/AppNavigator.tsx
+++ rnThai1/src/AppNavigator.tsx
@@ -1,13 +1,17 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen';
 import RegisterScreen from './RegisterScreen';
 import JSONFeedScreen from './JSONFeedScreen';
 import CameraScreen from './CameraScreen';
 import {RootStackParamList, RootTabParamList} from './RootNavigationParams';
 import ExtraScreen from './ExtraScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const defaultOptions = {
   headerStyle: {
@@ -20,11 +24,25 @@ const defaultOptions = {
 const Stack = createStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<RootTabParamList>();
 
+const tab1: BottomTabNavigationOptions = {
+  tabBarLabel: 'JSON',
+  tabBarIcon: ({focused}: any) => (
+    <Icon name="user" size={30} color={focused ? '#119CED' : '#0007'} />
+  ),
+};
+
+const tab2: BottomTabNavigationOptions = {
+  tabBarLabel: 'Camera',
+  tabBarIcon: ({focused}: any) => (
+    <Icon name="camera" size={30} color={focused ? '#119CED' : '#0007'} />
+  ),
+};
+
 const SuccessTab = () => (
-  <Tab.Navigator initialRouteName="Json">
-    <Tab.Screen name="Json" component={JSONFeedScreen} />
-    <Tab.Screen name="Camera" component={CameraScreen} />
-    <Tab.Screen name="Extra" component={ExtraScreen} />
+  <Tab.Navigator initialRouteName="Json" screenOptions={{headerShown: false}}>
+    <Tab.Screen name="Json" component={JSONFeedScreen} options={tab1} />
+    <Tab.Screen name="Camera" component={CameraScreen} options={tab2} />
+    {/* <Tab.Screen name="Extra" component={ExtraScreen} /> */}
   </Tab.Navigator>
 );
 
