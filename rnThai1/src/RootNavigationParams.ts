2afc5412e089496f9a18438d450068fb3fb98c7c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-06 09:39:14 +0700

    1234

---
+++ rnThai1/src/RootNavigationParams.ts
@@ -0,0 +1,10 @@
+export type RootStackParamList = {
+  Home: undefined;
+  Register: undefined;
+  Success: undefined | {username: string};
+};
+
+export type RootTabParamList = {
+  Json: undefined;
+  Camera: undefined;
+};
