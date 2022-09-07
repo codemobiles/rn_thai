774557580d943bd3e6bf22448b14083d44611185
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-07 13:19:46 +0700

    1234

--- rnThai1/App.tsx
+++ rnThai1/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {View} from 'react-native';
 
@@ -7,8 +9,14 @@ import AppNavigator from './src/AppNavigator';
 type Props = {};
 
 const App = (props: Props) => {
+  const [isAuthened, setIsAuthened] = React.useState(false);
+  const [isAuthenticating, setIsAuthenticating] = React.useState(true);
+  const navigation = useNavigation();
 
-  
+  React.useEffect(() => {
+    checkSession();
+  });
+
   return (
     <View style={{flex: 1}}>
       <AppNavigator isAuthened={true} />
