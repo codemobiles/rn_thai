f802f5238c4ab7e759687c5aaff1ec9689f41af6
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-07 14:08:06 +0700

    1234

--- rnThai1/src/store/slices/camera.slice.ts
+++ rnThai1/src/store/slices/camera.slice.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {Alert} from 'react-native';
 import {RootState} from '../store';
 
 export type cameraState = {
@@ -9,8 +10,12 @@ export type cameraState = {
 const defaultState: cameraState = {
   image: null,
 };
-export const handleCamera = createAsyncThunk('camera/handleCamera', () => {});
-export const handleGallery = createAsyncThunk('camera/handleGallery', () => {});
+export const handleCamera = createAsyncThunk('camera/handleCamera', () => {
+  Alert.alert('Camera');
+});
+export const handleGallery = createAsyncThunk('camera/handleGallery', () => {
+  Alert.alert('Gallery');
+});
 
 const cameraSlice = createSlice({
   name: 'camera',
