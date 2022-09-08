65cbf27ba131971cb62924693749b44642f9ee9b
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 14:52:34 +0700

    1234

--- rnThai4/index.js
+++ rnThai4/index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import React, {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {NavigationContainer} from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+const NavApp = () => (
+  <NavigationContainer>
+    <App />
+  </NavigationContainer>
+);
+AppRegistry.registerComponent(appName, () => NavApp);
