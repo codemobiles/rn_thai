259bff979fc0ff583402781e7192bbd5d6e52a0c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 13:36:20 +0700

    124

--- rnThai3/App.tsx
+++ rnThai3/App.tsx
@@ -1,67 +1,229 @@
-import {View, Text, Dimensions} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import {Button, Dimensions, ScrollView, Text, View} from 'react-native';
 import {
   LineChart,
   BarChart,
   PieChart,
   ProgressChart,
-  ContributionGraph,
-  StackedBarChart,
 } from 'react-native-chart-kit';
 
-type Props = {};
+interface AppProps {}
 
-const App = (props: Props) => {
-  return (
+const getDummy = () => {
+  return [
+    Math.random() * 100,
+    Math.random() * 100,
+    Math.random() * 100,
+    Math.random() * 100,
+    Math.random() * 100,
+    Math.random() * 100,
+  ];
+};
+
+const App: React.FunctionComponent<AppProps> = props => {
+  const [renderWidth, setRenderWidth] = React.useState(0);
+  const [dummyData1, setDummyData1] = React.useState<number[]>(getDummy());
+  const [dummyData2, setDummyData2] = React.useState<number[]>(getDummy());
+
+  React.useEffect(() => {
+    setRenderWidth(Dimensions.get('screen').width - 20);
+  }, []);
+
+  const drawLineChart = () => (
     <View>
-      <View>
-        <Text>Bezier Line Chart</Text>
-        <LineChart
-          data={{
-            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-            datasets: [
-              {
-                data: [
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                ],
-              },
-            ],
-          }}
-          width={Dimensions.get('window').width} // from react-native
-          height={220}
-          yAxisLabel="$"
-          yAxisSuffix="k"
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: '#e26a00',
-            backgroundGradientFrom: '#fb8c00',
-            backgroundGradientTo: '#ffa726',
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-              borderRadius: 16,
+      <Text>Line Chart</Text>
+      <LineChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              color: (opacity = 1) => `rgba(255, 0, 255, ${opacity})`,
+              data: dummyData1,
             },
-            propsForDots: {
-              r: '6',
-              strokeWidth: '2',
-              stroke: '#ffa726',
+
+            {
+              color: (opacity = 1) => `rgba(255, 255, 0, ${opacity})`,
+              data: dummyData2,
             },
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
+          ],
+        }}
+        width={renderWidth} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#333',
+          backgroundGradientTo: '#ffa726',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
             borderRadius: 16,
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#ffa726',
+          },
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </View>
+  );
+
+  const drawBarChart = () => (
+    <View>
+      <Text>Bar Chart</Text>
+      <BarChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={renderWidth}
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: '#026a00',
+          backgroundGradientFrom: '#026a00',
+          backgroundGradientTo: '#026a00',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#ffa726',
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </View>
+  );
+
+  const pieData = [
+    {
+      name: 'Seoul',
+      population: 21500000,
+      color: 'rgba(131, 167, 234, 1)',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Toronto',
+      population: 2800000,
+      color: '#F00',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Beijing',
+      population: 527612,
+      color: 'red',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'New York',
+      population: 8538000,
+      color: '#f000ff',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Moscow',
+      population: 11920000,
+      color: 'rgb(0, 0, 255)',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+  ];
+
+  const drawPieChart = () => (
+    <View>
+      <Text>Pie Chart</Text>
+      <PieChart
+        data={pieData}
+        width={renderWidth}
+        height={180}
+        chartConfig={chartConfig}
+        accessor={'population'}
+        backgroundColor={'transparent'}
+        center={[0, 0]}
+      />
+    </View>
+  );
+
+  const drawProgressChart = () => (
+    <View>
+      <Text>Progress Chart</Text>
+      <ProgressChart
+        data={{data: [0.4, 0.6, 0.8], labels: ['Swim', 'Bike', 'Run']}}
+        width={renderWidth}
+        height={220}
+        strokeWidth={16}
+        radius={32}
+        chartConfig={chartConfig}
+        hideLegend={false}
+      />
+    </View>
+  );
+
+  const chartConfig = {
+    backgroundGradientFrom: '#f0f',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: '#303',
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+
+  return (
+    <View style={{flex: 1, padding: 10}}>
+      <ScrollView
+        contentContainerStyle={{
+          flexDirection: 'column',
+        }}>
+        <Button
+          title="Load"
+          onPress={() => {
+            setDummyData1(getDummy());
+            setDummyData2(getDummy());
           }}
         />
-      </View>
+        {drawLineChart()}
+        {drawBarChart()}
+        {drawPieChart()}
+        {drawProgressChart()}
+      </ScrollView>
     </View>
   );
 };
-
 export default App;
