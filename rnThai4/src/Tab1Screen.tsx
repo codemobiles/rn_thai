a79f0ed44b6b33bfbcc73b94412d00d7b0a59cc6
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 15:20:09 +0700

    1234

--- rnThai4/src/Tab1Screen.tsx
+++ rnThai4/src/Tab1Screen.tsx
@@ -1,6 +1,34 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import MapView from 'react-native-maps';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import MapView, {
+  LatLng,
+  Marker,
+  PROVIDER_GOOGLE,
+  Region,
+} from 'react-native-maps';
+import openMap from 'react-native-open-maps';
+import CustomCallout from './CustomCallout';
+import {Callout} from 'react-native-maps';
+
+const {width, height} = Dimensions.get('window');
+const ipAddress = '192.168.1.178';
+import Axios from 'axios';
+
+const ASPECT_RATIO = width / height;
+// 13.6970244, 100.5130343 codemobiles office
+const LATITUDE = 35.6585848;
+const LONGITUDE = 139.7432442;
+const LATITUDE_DELTA = 0.0022;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 type Props = {};
 
