2c428c684d0fc06e8125cd91114fbf9b7b665d3b
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 10:11:58 +0700

    1234

--- rnThai2/src/TabQRcode.tsx
+++ rnThai2/src/TabQRcode.tsx
@@ -1,14 +1,97 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component, useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  ImageBackground,
+  ScrollView,
+  TextInput,
+} from 'react-native';
+import {ifIphoneX} from 'react-native-iphone-x-helper';
 
-type Props = {};
+import QRCode from 'react-native-qrcode-svg';
+const PATH_TO_LOGO = './img/cmdev_icon.png';
+interface TabQRcodeProps {}
 
-const TabQRcode = (props: Props) => {
+const TabQRcode: React.FunctionComponent<TabQRcodeProps> = props => {
+  const [qrValue, setQrValue] = useState('');
+
   return (
-    <View>
-      <Text>TabQRcode</Text>
-    </View>
+    <ImageBackground
+      source={require('./img/gradient_bg.png')}
+      resizeMode={'stretch'}
+      style={styles.container}>
+      <Image
+        resizeMode={'contain'}
+        style={{
+          width: '100%',
+          height: 120,
+          ...ifIphoneX({marginTop: 30}, {marginTop: 0}),
+          padding: 0,
+        }}
+        source={require('./img/header_react_native.png')}
+      />
+      <TextInput
+        onChangeText={text => setQrValue(text)}
+        placeholder="QRCode Value"
+        style={{
+          fontSize: 18,
+          height: 40,
+          textAlign: 'center',
+          borderWidth: 1,
+          borderColor: '#fff3',
+          borderRadius: 5,
+          marginLeft: 32,
+          marginRight: 32,
+          marginBottom: 32,
+          marginTop: 32,
+        }}
+      />
+      <ScrollView
+        contentContainerStyle={{
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        {[1, 2, 3, 4, 5, 6, 7, 8].map(item => (
+          <MyQRCode
+            key={String(item)}
+            value={qrValue}
+            color={item % 2 ? 'red' : 'green'}
+          />
+        ))}
+      </ScrollView>
+    </ImageBackground>
   );
 };
 
 export default TabQRcode;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'stretch',
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  section: {
+    marginTop: 15,
+    marginBottom: 15,
+  },
+});
+
+type MyQRCodeProps = {
+  value: string;
+};
+const MyQRCode: React.FC<MyQRCodeProps> = props => {
+  return (
+    <View style={{margin: 10}}>
+      <QRCode
+        {...props}
+        value={props.value !== '' ? props.value : 'www.codemobiles.com'}
+      />
+    </View>
+  );
+};
