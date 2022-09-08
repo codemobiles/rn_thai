1c15d84444f5bd2ebf06e289722b37883e555f94
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 16:24:11 +0700

    1234

--- rnThai4/backend/src/server.ts
+++ rnThai4/backend/src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 const app = express();
+app.use(express.json());
 
-app.get('./', (req, res) => {
+app.get('/', (req, res) => {
   res.json({result: 'Ok'});
+});
+
+app.post('/register', (req, res) => {
+  res.json({register: req.body});
 });
 
 app.listen(3000, () => console.log('server is running..'));
