5e8e21a4871f83109c7f34141adeb9c4e9899018
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 15:13:48 +0700

    1234

--- rnThai4/src/Tab1Screen.tsx
+++ rnThai4/src/Tab1Screen.tsx
@@ -1,14 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
 
-type Props = {}
+type Props = {};
 
 const Tab1Screen = (props: Props) => {
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={{flex: 1}}>
+      <MapView
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Tab1Screen
+export default Tab1Screen;
