fba38c8c43d14cb4f2fe5900266c99f7b3c3a369
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 16:32:57 +0700

    1234

--- rnThai4/backend/src/server.ts
+++ rnThai4/backend/src/server.ts
@@ -10,4 +10,16 @@ app.post('/register', (req, res) => {
   res.json({register: req.body});
 });
 
+let dataArray: any[] = [];
+
+app.post('/record_position', async (req, res) => {
+  console.log(JSON.stringify(req.body));
+  const data = {...req.body, id: dataArray.length};
+  dataArray.push(data);
+  res.json({result: 'ok'});
+});
+
+app.get('/position', async (req, res) => {
+  res.json(dataArray);
+});
 app.listen(3000, () => console.log('server is running..'));
