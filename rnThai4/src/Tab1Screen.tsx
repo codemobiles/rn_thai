7019365b46f663c2b937a113a916da746fbf2ba5
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 14:18:19 +0700

    1234

---
+++ rnThai4/src/Tab1Screen.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const Tab1Screen = (props: Props) => {
+  return (
+    <View>
+      <Text>Tab1Screen</Text>
+    </View>
+  );
+};
+
+export default Tab1Screen;
---
+++ rnThai4/src/Tab2Screen.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const Tab2Screen = (props: Props) => {
+  return (
+    <View>
+      <Text>Tab2Screen</Text>
+    </View>
+  );
+};
+
+export default Tab2Screen;
