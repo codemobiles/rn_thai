cd10962a80e72215effd9dc5dd3983d816776ff8
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 16:17:31 +0700

    1234

--- rnThai4/backend/src/server.ts
+++ rnThai4/backend/src/server.ts
@@ -1,2 +1,8 @@
-const tmp: string = '555';
-console.log('1234');
+import express from 'express';
+const app = express();
+
+app.get('./', (req, res) => {
+  res.json({result: 'Ok'});
+});
+
+app.listen(3000, () => console.log('server is running..'));
