0ff8a300fef21e15ffa18c36cf7f63cac3d24504
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-08 10:56:02 +0700

    1234

--- rnThai2/src/ScannerScreen.tsx
+++ rnThai2/src/ScannerScreen.tsx
@@ -1,9 +1,15 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {useRoute} from '@react-navigation/native';
 
 type Props = {};
 
 const ScannerScreen = (props: Props) => {
+  const route = useRoute();
+  React.useEffect(() => {
+    console.log(route.params?.token);
+  });
+
   return (
     <View>
       <Text>ScannerScreen</Text>
